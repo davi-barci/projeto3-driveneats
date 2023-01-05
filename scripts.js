a6a216eddb0164e02e1db2edf85f564c09069127
Davi Barci <davibarci0208@gmail.com>
2023-01-05 17:47:04 -0300

    Versão 3.0 - Aplicado ajustes no botão de finalizar o pedido

--- scripts.js
+++ scripts.js
@@ -1,9 +1,22 @@
+function finalizarPedido(){
+    console.log("teste...");
+}
+
 function selecionandoItens(tipoRefeicao,objetoSelecionado){
     const selecaoAtual = document.querySelectorAll(tipoRefeicao+'.selecionado');
-    console.log(selecaoAtual);
     if (selecaoAtual.length !== 0){
         selecaoAtual[0].classList.remove('selecionado');
     }
     const refeicaoSelecionada = document.querySelector(objetoSelecionado);
-    refeicaoSelecionada .classList.add('selecionado');
+    refeicaoSelecionada.classList.add('selecionado');
+
+    const qtdSelecoes = document.querySelectorAll('.selecionado');
+    if (qtdSelecoes.length === 3){
+        const botaoPedido = document.querySelector('.fechamento-pedido');
+        botaoPedido.addEventListener("click", finalizarPedido);
+        document.querySelector('.fechamento-pedido div').style.backgroundColor = "#32B72F";
+        let textoBotao = document.querySelector('.fechamento-pedido p');
+        textoBotao.style.fontWeight = "700";
+        textoBotao.innerHTML = "Fechar Pedido"
+    }
 }
