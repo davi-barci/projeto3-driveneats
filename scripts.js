d1adbddfcb80abd589c34bf96620fb114777db26
Davi Barci <davibarci0208@gmail.com>
2023-01-06 00:43:01 -0300

    Versão 4.0 - Adicionada a funcionalidade de envio do pedido para o whatsapp do restaurante

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
+let itens = [], precoItens = [];
+
 function finalizarPedido(){
-    console.log("teste...");
+    let precoTotal = precoItens.reduce((a, b) => {
+        return a + b;
+      });
+    let mensagem = "Olá, gostaria de fazer o pedido:" + "\n" + 
+    "- Prato: " + itens[0] + "\n" +
+    "- Bebida: " + itens[1] + "\n" +
+    "- Sobremesa: " + itens[2] + "\n" +
+    "Total: R$ " + precoTotal.toFixed(2);
+
+    window.open("https://wa.me/5521979065971?text=" + encodeURIComponent(mensagem));
+}
+
+function transformarPrecoDecimal(texto){
+    texto = texto.slice(3,texto.length);
+    texto = texto.replace(',','.');
+    texto = Number(texto);
+    return texto;
 }
 
 function selecionandoItens(tipoRefeicao,objetoSelecionado){
@@ -7,8 +25,22 @@ function selecionandoItens(tipoRefeicao,objetoSelecionado){
     if (selecaoAtual.length !== 0){
         selecaoAtual[0].classList.remove('selecionado');
     }
-    const refeicaoSelecionada = document.querySelector(objetoSelecionado);
-    refeicaoSelecionada.classList.add('selecionado');
+
+    objetoSelecionado.classList.add('selecionado');
+
+    if (tipoRefeicao === '.prato'){
+        itens[0] = document.querySelector('.prato.selecionado p').innerHTML;
+        let precoTexto = document.querySelector('.prato.selecionado p:nth-of-type(3)').innerHTML;
+        precoItens[0] = transformarPrecoDecimal(precoTexto);
+    }else if (tipoRefeicao === '.bebida'){
+        itens[1] = document.querySelector('.bebida.selecionado p').innerHTML;
+        let precoTexto = document.querySelector('.bebida.selecionado p:nth-of-type(3)').innerHTML;
+        precoItens[1] = transformarPrecoDecimal(precoTexto);
+    }else{
+        itens[2] = document.querySelector('.sobremesa.selecionado p').innerHTML;
+        let precoTexto = document.querySelector('.sobremesa.selecionado p:nth-of-type(3)').innerHTML;
+        precoItens[2] = transformarPrecoDecimal(precoTexto);
+    }
 
     const qtdSelecoes = document.querySelectorAll('.selecionado');
     if (qtdSelecoes.length === 3){
